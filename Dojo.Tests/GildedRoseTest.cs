eb2e92dfec903fcf31f9334952a469392901fc0c
André Batista <S01340@newday.co.uk>
2022-03-08 14:56:01 +0000

    Add tests

--- Dojo.Tests/GildedRoseTest.cs
+++ Dojo.Tests/GildedRoseTest.cs
@@ -7,12 +7,27 @@ namespace GildedRoseTests
     public class GildedRoseTest
     {
         [Fact]
-        public void foo()
+        public void Quality_Should_Decrease_At_The_End_Of_The_Day()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "Test item", SellIn = 10, Quality = 10 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+
+            var expectedQuality = 9;
+
+            Assert.Equal(expectedQuality, Items[0].Quality);
+        }
+
+        [Fact]
+        public void Quality_Should_Decrease_Twice_As_Fast_When_Sell_By_Date_Has_Passed()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Test item", SellIn = 0, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            var expectedQuality = 8;
+
+            Assert.Equal(expectedQuality, Items[0].Quality);
         }
     }
 }
