08023ca7e44e501f4836b2c9f6e6b12debd12b42
André Batista <S01340@newday.co.uk>
2022-03-08 15:42:00 +0000

    Add tests Add test data

--- Dojo.Tests/GildedRoseTest.cs
+++ Dojo.Tests/GildedRoseTest.cs
@@ -146,7 +146,30 @@ namespace GildedRoseTests
 
             var expectedSellIn = 10;
 
-            Assert.Equal(expectedSellIn, Items[0].Quality);
+            Assert.Equal(expectedSellIn, Items[0].SellIn);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData.Sulfras_Test_Data), MemberType = typeof(TestData))]
+        public void Sulfuras_Never_Decreases_In_Quality_Or_SellIn(List<Item> items, int expectedSellIn, int expectedQuality)
+        {
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+
+            Assert.Equal(expectedSellIn, items[0].SellIn);
+            Assert.Equal(expectedQuality, items[0].Quality);
+        }
+
+        [Fact]
+        public void Backstage_Passes_Quality_Should_Increase_As_SellIn_Is_10_Days_Or_Less()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            var expectedQuality = 12;
+
+            Assert.Equal(expectedQuality, Items[0].Quality);
         }
     }
 }
---
+++ Dojo.Tests/TestData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRoseKata
+{
+    public class TestData
+    {
+        public static IEnumerable<object[]> Sulfras_Test_Data()
+        {
+            yield return new object[]
+            {
+                new List<Item>
+                {
+                    new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 10 }
+                }, 10, 10
+            };
+        }
+    }
+}
